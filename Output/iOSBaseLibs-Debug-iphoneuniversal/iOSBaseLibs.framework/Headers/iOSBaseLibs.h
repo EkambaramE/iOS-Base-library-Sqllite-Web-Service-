deb7971fc2ad24cf9032450b20b2c6fc1af6c77f
Ekambaram E <eekambaram90@gamil.com>
2016-05-02 11:15:17 +0530

    first commit

---
+++ Output/iOSBaseLibs-Debug-iphoneuniversal/iOSBaseLibs.framework/Headers/iOSBaseLibs.h
@@ -0,0 +1,21 @@
+//
+//  iOSBaseLibs.h
+//  iOSBaseLibs
+//
+//  Created by Muthuraj M on 30/09/15.
+//  Copyright (c) 2015 Muthuraj M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ServiceHandler.h"
+#import "DataOperation.h"
+
+//! Project version number for iOSBaseLibs.
+FOUNDATION_EXPORT double iOSBaseLibsVersionNumber;
+
+//! Project version string for iOSBaseLibs.
+FOUNDATION_EXPORT const unsigned char iOSBaseLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSBaseLibs/PublicHeader.h>
+
+
